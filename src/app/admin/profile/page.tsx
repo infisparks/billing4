14f4867135df597297de85f7dd0e77837bfff802
mk <infisparks@gmail.com>
2025-01-07 22:05:18 -0500

    asdf

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Banner from 'components/admin/profile/Banner';
 import General from 'components/admin/profile/General';
-import Notification from 'components/admin/profile/Notification';
+
 import Project from 'components/admin/profile/Project';
 import Storage from 'components/admin/profile/Storage';
 import Upload from 'components/admin/profile/Upload';
@@ -33,7 +33,7 @@ const ProfileOverview = () => {
         </div>
 
         <div className="col-span-5 lg:col-span-12 lg:mb-0 3xl:!col-span-3">
-          <Notification />
+        
         </div>
       </div>
     </div>
