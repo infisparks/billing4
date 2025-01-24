f4b36e2c35ce500f732d745664a3f6cf99b63571
MK MUZAKKIR <muzakkir7977@gmail.com>
2025-01-24 14:45:54 -0500

    sfd

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -5,14 +5,14 @@ import { getAuth, Auth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBeJ2KXmPjFwskO7zqYNcb9hFdgZcr7mv4",
+  authDomain: "muzakkir-e7ea9.firebaseapp.com",
+  databaseURL: "https://muzakkir-e7ea9-default-rtdb.firebaseio.com",
+  projectId: "muzakkir-e7ea9",
+  storageBucket: "muzakkir-e7ea9.appspot.com",
+  messagingSenderId: "170728036106",
+  appId: "1:170728036106:web:911d8afbf0a85b273a4cf3",
+  measurementId: "G-B8QM684X5Q"
 };
 
 // Initialize Firebase
